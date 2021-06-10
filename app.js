8f5ff8dfbbb9da59b853dffcfd29fdebea19e6f6
Salsabaga <dbaldeonabril@gmail.com>
2021-06-10 17:37:14 +0100

    Tooltips and adjustment of front page and landing page

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const constituencies = document.getElementsByTagName("path");
-const constituencyArr = Array.from(constituencies);
+// Fetch Data function
 
 async function mpData(entry) {
 	const response = await fetch(
@@ -10,51 +9,95 @@ async function mpData(entry) {
 		data.items[0].value.currentRepresentation.member.value.nameFullTitle;
 	const imageMP = await data.items[0].value.currentRepresentation.member.value
 		.thumbnailUrl;
-	$("#loading-area").hide();
-	$("#member-pic > img").attr("src", `${imageMP}`);
-	$("#member-name").text(fullNameMP);
-	$("#member-area").text(entry.id);
-	const mpParty = data.items[0].value.currentRepresentation.member.value.latestParty.name
-
 	const mpPartyAbr =
 		data.items[0].value.currentRepresentation.member.value.latestParty
 			.abbreviation;
+	$("#loading-area").removeClass("active");
+	$("#member-link").css("display", "block");
+	$("#member-pic > img").attr("src", `${imageMP}`);
+	$("#member-name").text(fullNameMP);
+	$("#member-area").text(entry.id);
 	if (mpPartyAbr === "Lab") {
-		$("#member-info").css("background-color", "var(--labour)");
-		const resLab = await fetch(`public/svg/${mpParty.replace(mpParty, "Labour")}.svg`);
-		const svgLab = await resLab.text();
-		$("#member-svg").html(svgLab);
+		$("#member-card").css("background-color", "var(--labour)");
+		$("#member-svg img").attr("src", "public/svg/Labour.svg");
 	} else if (mpPartyAbr === "Con") {
-		$("#member-info").css("background-color", "var(--conservative)");
-		const resLab = await fetch(`public/svg/${mpParty}.svg`);
-		const svgLab = await resLab.text();
-		$("#member-svg").html(svgLab);
+		$("#member-card").css("background-color", "var(--conservative)");
+		$("#member-svg img").attr("src", "public/svg/Conservative.svg");
 	} else if (mpPartyAbr === "LD") {
-		$("#member-info").css("background-color", "var(--lib-dems)");
-		const resLab = await fetch(`public/svg/Liberal-Democrat.svg`);
-		const svgLab = await resLab.text();
-		$("#member-svg").html(svgLab);
+		$("#member-card").css("background-color", "var(--lib-dems)");
+		$("#member-svg img").attr("src", "public/svg/Liberal-Democrat.svg");
 	} else {
 		console.log("Invalid User");
 	}
-	console.log(data);
-
 }
+
+function show(e) {
+	mpData(e.target);
+	$("#loading-area").addClass("active");
+	$(e.target.innerHTML).css("fill", "#448fda");
+}
+
+const constituencies = document.getElementsByTagName("path");
+const constituencyArr = Array.from(constituencies);
+const search = document.getElementById("mpSearch");
+const constituencyList = document.getElementById("match-list");
+const spinner = document.querySelector("#loading-area");
+
+// Search Bar
+
+constituencyList.innerHTML = constituencyArr
+	.map(
+		(match) =>
+			`<div id="${match.id}" class="suggestions" onclick="show(event)">${match.id}</div>`
+	)
+	.join("");
+
+search.addEventListener("input", (e) => {
+	let matches = constituencyArr.filter((constit) => {
+		const regex = new RegExp(`^${e.target.value}`, "gi");
+		return constit.id.match(regex);
+	});
+
+	constituencyList.innerHTML = matches
+		.map(
+			(match) =>
+				`<div id="${match.id}" class="suggestions" onclick="show(event)">${match.id}</div>`
+		)
+		.join("");
+});
+
+// Hover over the constituencies
 
 $(document).ready(() => {
 	constituencyArr.forEach((c) => {
 		$(c)
 			.mouseover(() => {
 				$(c).css("fill", "hsl(0, 100%, 100%)");
-				$("#testing-area > p").text(c.id);
 			})
 			.mouseleave(() => {
 				$(c).css("fill", "#17b794");
-				$("#testing-area > p").text("Hover over a constituency");
 			})
 			.click(() => {
-				$("#loading-area").animate({ height: "toggle" });
+				$(c).css("fill", "#b89bff");
+				$("#loading-area").addClass("active");
 				mpData(c);
 			});
+	});
+});
+
+// Tooltip
+
+const tooltip = document.querySelector(".map-tooltip");
+
+[].forEach.call(constituencyArr, function (item) {
+	item.addEventListener("mousemove", function (e) {
+		tooltip.style.display = "block";
+		tooltip.style.top = 10 + e.clientY + "px";
+		tooltip.style.left = 20 + e.clientX + "px";
+		tooltip.textContent = e.target.getAttribute("id");
+	});
+
+	item.addEventListener("mouseleave", () => {
+		tooltip.style.display = "none";
 	});
 });
